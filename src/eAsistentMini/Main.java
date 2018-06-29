da92b5d67b3c12943ccf142d18fddfa69d84c5f9
Gregor Klinc <gregor.klinc99@gmail.com>
2018-06-29 14:42:24 +0200

    think this works?

--- src/eAsistentMini/Main.java
+++ src/eAsistentMini/Main.java
@@ -6,20 +6,42 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 import static javafx.application.Application.launch;
 
 public class Main extends Application {
-    int ActiveSceene=0;
+    int ActiveSceene=4;
+    Stage window;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+        window=primaryStage;
+//        Parent login = FXMLLoader.load(getClass().getResource("login.fxml"));
+        selectedStage(ActiveSceene,window);
+        window.setTitle("Login");
+        window.show();
+
+
+    }
+    public void selectedStage(int selectedSceene,Stage stage) throws IOException {
         Parent login = FXMLLoader.load(getClass().getResource("login.fxml"));
         Parent admin= FXMLLoader.load(getClass().getResource("admin.fxml"));
         Parent teacherMain= FXMLLoader.load(getClass().getResource("teacherMain.fxml"));
         Parent userMain= FXMLLoader.load(getClass().getResource("userMain.fxml"));
-        primaryStage.setTitle("eAsistentMini");
-        primaryStage.setScene(new Scene(login, 300, 275));
-        primaryStage.show();
+        if(selectedSceene==0){
+            stage.setScene(new Scene(admin));
+        }else
+            if(selectedSceene==1){
+            stage.setScene(new Scene(teacherMain));
+        }else
+            if(selectedSceene==2){
+            stage.setScene(new Scene(userMain));
+        }else{
+                stage.setScene(new Scene(login));
+            }
+
     }
     public static void main(String[] args) {
         launch(args);
--- src/eAsistentMini/TeacherController.java
+++ src/eAsistentMini/TeacherController.java
@@ -1,4 +1,21 @@
 package eAsistentMini;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class TeacherController {
+    public TableView studentsTable;
+    public TextField gradeField;
+    public Button addGradeButton;
+    public Button logOffButton;
+
+    public void addButtonPressed(ActionEvent actionEvent) {
+
+    }
+
+    public void logOffButtonPressed(ActionEvent actionEvent) {
+
+    }
 }
